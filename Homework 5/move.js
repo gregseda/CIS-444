7050722ca1ea9e5b0f0c486ca2ec9f1b0a0074c2
gregseda <31677674+gregseda@users.noreply.github.com>
2017-12-27 13:09:57 -0800

    Add files via upload

---
+++ Homework 5/move.js
@@ -0,0 +1,77 @@
+/*
+	Greg Seda
+	CIS 444 Homework 5
+	move.js
+	http://cis444.cs.csusm.edu/seda004/hw5/move.js
+*/
+
+var dom;            
+var x;              
+var start;          
+var end;            
+var changes = false; 
+
+function initText() {
+  dom = document.getElementById('statement');
+    
+  start = parseInt(document.getElementById('startCoord').value);
+  end = parseInt(document.getElementById('endCoord').value);
+  document.getElementById('endCoord').max = screen.width.toString();
+
+
+  setInterval(function() {moveTheText(); }, 1);
+}
+
+function moveTheText() {
+  x = dom.style.left;
+  x = x.match(/\d+/);
+  
+  if (!changes)
+    moveTextLeft(); 
+  else
+    moveTextRight(); 
+  
+  flashTheText(x);
+}
+
+function moveTextRight() {
+  if (x > start)
+    x--;
+    
+  dom.style.left = x + "px";
+  
+  if (x == start)
+    changes = false;
+}
+
+function moveTextLeft(){
+  if(x < end)
+    x++;
+  
+  dom.style.left = x + "px";
+  
+  if(x == end)
+    changes = true;
+  }
+
+function flashTheText(pos) {
+  if(pos % 5 == 0) {
+    if(dom.style.color == "red")
+      dom.style.color = "blue";
+    else
+      dom.style.color = "red";
+  }
+}
+
+function setRange() {
+  start = parseInt(document.getElementById('startCoord').value);
+  end = parseInt(document.getElementById('endCoord').value);
+  
+  dom.style.left = start + "px";
+  changes = false;
+}
+
+function setMinMax() {
+  document.getElementById("startCoord").max = document.getElementById("endCoord").value;
+  document.getElementById("endCoord").min = document.getElementById("startCoord").value;
+}
